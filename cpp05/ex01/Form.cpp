0a11c6aeb6d44ec8f13bc3651477da69565dd3dc
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 16:51:55 +0200

    Update Form.cpp

--- cpp05/ex01/Form.cpp
+++ cpp05/ex01/Form.cpp
@@ -13,16 +13,34 @@
 #include "Form.hpp"
 #include "Bureaucrat.hpp"
 
+/**
+ * @brief Exception message for grade too low in Form.
+ * 
+ * @return const char* Error message string.
+ */
 const char* Form::GradeTooLowException::what() const throw()
 {
 	return ("Grade is too low");
 }
 
+/**
+ * @brief Exception message for grade too high in Form.
+ * 
+ * @return const char* Error message string.
+ */
 const char* Form::GradeTooHighException::what() const throw()
 {
 	return ("Grade is too high");
 }
 
+/**
+ * @brief Attempts to sign the form by the given Bureaucrat.
+ * 
+ * Throws GradeTooLowException if bureaucrat's grade is too low.
+ * Sets isSigned flag to true on success.
+ * 
+ * @param bureaucrat Bureaucrat attempting to sign the form.
+ */
 void Form::beSigned( Bureaucrat bureaucrat )
 {
 	if ( this->gradeToSign < bureaucrat.getGrade())
@@ -30,26 +48,55 @@ void Form::beSigned( Bureaucrat bureaucrat )
 	this->isSigned = 1;
 }
 
+/**
+ * @brief Gets the name of the form.
+ * 
+ * @return std::string The form's name.
+ */
 const std::string	Form::getName() const
 {
 	return (this->name);
 }
 
+/**
+ * @brief Returns whether the form is signed.
+ * 
+ * @return true if signed, false otherwise.
+ */
 bool Form::getIsSigned() const
 {
 	return (this->isSigned);
 }
 
+/**
+ * @brief Gets the grade required to sign the form.
+ * 
+ * @return int Grade required for signing.
+ */
 const int Form::getGradeToSign() const
 {
 	return (this->gradeToSign);
 }
 
+/**
+ * @brief Gets the grade required to execute the form.
+ * 
+ * @return int Grade required for execution.
+ */
 const int Form::getGradeToExecute() const
 {
 	return (this->gradeToExecute);
 }
 
+/**
+ * @brief Copy assignment operator overload.
+ * 
+ * Copies all fields from another Form, including const members
+ * via const_cast.
+ * 
+ * @param before Form to copy from.
+ * @return Form& Reference to this object.
+ */
 Form& Form::operator=( const Form& before )
 {
 	std::cout << "Copy Form Assigment operator called" << std::endl;
@@ -60,6 +107,13 @@ Form& Form::operator=( const Form& before )
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor.
+ * 
+ * Copies all data from another Form, throws if grades invalid.
+ * 
+ * @param before Form to copy from.
+ */
 Form::Form( const Form& before ) : name(before.name), gradeToSign(before.gradeToSign), gradeToExecute(before.gradeToExecute)
 {
 	this->isSigned = before.isSigned;
@@ -71,6 +125,16 @@ Form::Form( const Form& before ) : name(before.name), gradeToSign(before.gradeTo
 	*this = before;
 }
 
+/**
+ * @brief Parameterized constructor.
+ * 
+ * Initializes form with name and required grades, sets signed false.
+ * Throws if grades are out of valid range [1,150].
+ * 
+ * @param name Name of the form.
+ * @param gradeToSign Grade required to sign.
+ * @param gradeToExecute Grade required to execute.
+ */
 Form::Form( std::string name, int gradeToSign, int gradeToExecute ) : name(name), gradeToSign(gradeToSign), gradeToExecute(gradeToExecute)
 {
 	std::cout << "Form " << name << " Constructor called" << std::endl;
@@ -81,17 +145,34 @@ Form::Form( std::string name, int gradeToSign, int gradeToExecute ) : name(name)
 		throw (Form::GradeTooLowException());
 }
 
+/**
+ * @brief Default constructor.
+ * 
+ * Initializes a form with default values and unsigned status.
+ */
 Form::Form() : name(""), gradeToExecute(150), gradeToSign(150)
 {
 	this->isSigned = 0;
 	std::cout << "Default Form Constructor called" << std::endl;
 }
 
+/**
+ * @brief Destructor.
+ */
 Form::~Form()
 {
 	std::cout << "Default Form Destructor called" << std::endl;
 }
 
+/**
+ * @brief Stream insertion operator overload.
+ * 
+ * Prints form details: name, grade required to sign and execute.
+ * 
+ * @param out Output stream.
+ * @param classToPrint Form to print.
+ * @return std::ostream& Reference to output stream.
+ */
 std::ostream&	operator<<( std::ostream &out, Form const &classToPrint )
 {
 	out << classToPrint.getName() << ", Form grade to sign: " << classToPrint.getGradeToSign() << " grade to execute: " << classToPrint.getGradeToExecute() << std::endl;
