3a27ce6bf2679717688b9cc1eba03b3ac5e0a2ba
isainz-r <162354189+ines-sainz@users.noreply.github.com>
2025-06-16 17:32:24 +0200

    Update RobotomyRequestForm.cpp

--- cpp05/ex03/RobotomyRequestForm.cpp
+++ cpp05/ex03/RobotomyRequestForm.cpp
@@ -14,6 +14,14 @@
 #include "Bureaucrat.hpp"
 #include <ctime>
 
+/**
+ * @brief Executes the Robotomy Request form.
+ * 
+ * Throws GradeTooLowException if executor's grade is too low.
+ * Randomly announces success or failure of the robotomy on the target.
+ * 
+ * @param executor The Bureaucrat executing the form.
+ */
 void RobotomyRequestForm::execute(Bureaucrat const & executor) const
 {
 	if (getGradeToExecute() < executor.getGrade())
@@ -27,6 +35,14 @@ void RobotomyRequestForm::execute(Bureaucrat const & executor) const
 		std::cout << "Target " << target << "robotomy has failed" << std::endl;
 }
 
+/**
+ * @brief Assignment operator overload.
+ * 
+ * Copies the target from another RobotomyRequestForm.
+ * 
+ * @param before The form to copy from.
+ * @return RobotomyRequestForm& Reference to this form.
+ */
 RobotomyRequestForm& RobotomyRequestForm::operator=( const RobotomyRequestForm& before )
 {
 	std::cout << "Copy RobotomyRequestForm Assigment Operator called" << std::endl;
@@ -34,24 +50,49 @@ RobotomyRequestForm& RobotomyRequestForm::operator=( const RobotomyRequestForm& 
 	return (*this);
 }
 
+/**
+ * @brief Copy constructor.
+ * 
+ * Copies all members including base class from another form.
+ * 
+ * @param before The form to copy.
+ */
+
 RobotomyRequestForm::RobotomyRequestForm( const RobotomyRequestForm& before ) : AForm(before.getName(), before.getGradeToSign(), before.getGradeToExecute()), target(before.target)
 {
 	std::cout << "Copy RobotomyRequestForm Constructor called" << std::endl;
 	*this = before;
 }
 
+/**
+ * @brief Parameterized constructor.
+ * 
+ * Creates a form with a given target, and fixed name and grade values.
+ * 
+ * @param target The target to robotomize.
+ */
 RobotomyRequestForm::RobotomyRequestForm( std::string target ) : AForm("RobotomyRequestForm", 72, 45)
 {
 	this->target = target;
 	std::cout << "RobotomyRequestForm Constructor called" << std::endl;
 }
 
+/**
+ * @brief Default constructor.
+ * 
+ * Creates a form with default target and fixed name and grades.
+ */
 RobotomyRequestForm::RobotomyRequestForm( void ) : AForm("RobotomyRequestForm", 72, 45), target("target")
 {
 	this->target = "target";
 	std::cout << "Default RobotomyRequestForm Constructor called" << std::endl;
 }
 
+/**
+ * @brief Destructor.
+ * 
+ * Prints a message when the form is destructed.
+ */
 RobotomyRequestForm::~RobotomyRequestForm()
 {
 	std::cout << "Default RobotomyRequestForm Destructor called" << std::endl;
